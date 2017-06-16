e8c60744ea04a4946f926f6e414b030a9e99c967
Ziming Li <reymondli0510@gmail.com>
2017-06-16 16:03:42 -0400

    Initial Commit

---
+++ SecondMeme/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecondMeme
+//
+//  Created by ziming li on 2017-06-16.
+//  Copyright © 2017 ziming li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
